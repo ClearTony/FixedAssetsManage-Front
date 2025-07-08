da7de754e2fb92e4252e89d0919b3b540eb2239b
ClearTony <903984996@qq.com>
2025-07-08 09:58:55 +0800

    初始化项目

---
+++ src/views/manager/Home.vue
@@ -0,0 +1,94 @@
+<template>
+  <div>
+    <div class="card" style="padding: 15px">
+      您好，{{ user?.name }}！欢迎使用本系统
+    </div>
+
+    <div style="display: flex; grid-gap: 10px; margin: 10px 0">
+      <div style="width: 50%;" class="card">
+        <div style="margin-bottom: 30px; font-size: 20px; font-weight: bold">公告列表</div>
+        <div >
+          <el-timeline  reverse slot="reference">
+            <el-timeline-item v-for="item in notices" :key="item.id" :timestamp="item.time">
+              <el-popover
+                  placement="right"
+                  width="200"
+                  trigger="hover"
+                  :content="item.content">
+                <span slot="reference">{{ item.title }}</span>
+              </el-popover>
+            </el-timeline-item>
+          </el-timeline>
+        </div>
+      </div>
+
+      <div style="width: 50%;" class="card">
+        <div id="pie" style="height: 400px"></div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import * as echarts from 'echarts'
+const pieOption = {
+  title: {
+    text: '分类资产统计',
+    subtext: '比例图',
+    left: 'center'
+  },
+  tooltip: {
+    trigger: 'item'
+  },
+  legend: {
+    orient: 'vertical',
+    left: 'left'
+  },
+  series: [
+    {
+      type: 'pie',
+      center: ['50%', '60%'],
+      radius: '50%',
+      data: [],
+      label: {
+        show: true,
+        formatter(param) {
+          return param.name + ' (' + param.percent + '%)';
+        }
+      },
+      emphasis: {
+        itemStyle: {
+          shadowBlur: 10,
+          shadowOffsetX: 0,
+          shadowColor: 'rgba(0, 0, 0, 0.5)'
+        }
+      }
+    }
+  ]
+}
+
+export default {
+  name: 'Home',
+  data() {
+    return {
+      user: JSON.parse(localStorage.getItem('xm-user') || '{}'),
+      notices: []
+    }
+  },
+  created() {
+    this.$request.get('/notice/selectAll').then(res => {
+      this.notices = res.data || []
+    })
+  },
+  mounted() {
+    // 饼图
+    let pieDom = document.getElementById('pie');
+    let pieChart = echarts.init(pieDom);
+
+    this.$request.get('/selectPie').then(res => {
+      pieOption.series[0].data = res.data || []
+      pieChart.setOption(pieOption)
+    })
+  }
+}
+</script>
